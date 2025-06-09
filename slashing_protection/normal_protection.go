a7dacba7a2064bf444f3f76600cc6abf81726cf7
oleg-ssvlabs <oleg.iar@ssvlabs.io>
2025-06-09 11:29:50 +0200

    Debugging

--- slashing_protection/normal_protection.go
+++ slashing_protection/normal_protection.go
@@ -38,9 +38,10 @@ func (protector *NormalProtection) IsSlashableAttestation(pubKey []byte, attesta
 		// We prevent double voting by rejecting another attestations with the same target epoch
 		// however you are eligible to sign the message with the same target epoch and the signing root,
 		// we are being strict by not storing the signing roots
-		fmt.Printf("ATTESTATION SOURCE EPOCH: %d, VALIDATOR SOURCE EPOCH: %d, ATTESTATION TARGET EPOCH: %d, VALIDATOR TARGET EPOCH: %d",
+		fmt.Printf("DEBUGGING_STUFF: ATTESTATION SOURCE EPOCH: %d, VALIDATOR SOURCE EPOCH: %d, ATTESTATION TARGET EPOCH: %d, VALIDATOR TARGET EPOCH: %d",
 			attestation.Source.Epoch, highest.Source.Epoch, attestation.Target.Epoch, highest.Target.Epoch)
 
+		//NOTE: this check returns true because highest.Source.Epoch is uint64 max
 		if attestation.Source.Epoch < highest.Source.Epoch || attestation.Target.Epoch <= highest.Target.Epoch {
 			return &core.AttestationSlashStatus{
 				Attestation: attestation,
